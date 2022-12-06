f86c687ba2d80dc01bef972f3d0643a01592805a
Colin <colin.rogers@dbtlabs.com>
2022-12-06 12:07:41 -0800

    add wheel and sys packages

--- release_creation/snapshot/download.sh
+++ release_creation/snapshot/download.sh
@@ -2,9 +2,10 @@ set -e
 pyenv install $3 --skip-existing
 install_env=".snapshot-env-$3"
 pyenv local $3
-python -mvenv $install_env
+python -mvenv $install_env --system-site-packages
 . $install_env/bin/activate
 python -m pip install --upgrade pip
+python -m pip install wheel
 rm -rf $1
 mkdir  -p $1
 python -m pip download \
