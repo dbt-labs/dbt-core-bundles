11179123b04b23cb2f75125a94a6f41d3a67ef95
Colin <colin.rogers@dbtlabs.com>
2022-12-06 11:32:17 -0800

    use venv instead pyenv-virtualenv

--- release_creation/snapshot/download.sh
+++ release_creation/snapshot/download.sh
@@ -1,10 +1,13 @@
 set -e
 pyenv install $3 --skip-existing
-pyenv virtualenv $3 snapshot-env -f
-pyenv local snapshot-env
+install_env=".snapshot-env-$3"
+pyenv local $3
+python -mvenv $install_env
+source $install_env/bin/activate
 python -m pip install --upgrade pip
 rm -rf $1
 mkdir $1
 python -m pip download \
  --dest $1 \
  $2
+source deactivate
