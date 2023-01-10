3c0210739b6a33f8ab09b3893b8238d0d4eb0015
Colin <colin.rogers@dbtlabs.com>
2023-01-10 13:07:43 -0800

    adapt sed command to be os specific

--- release_creation/snapshot/freeze.sh
+++ release_creation/snapshot/freeze.sh
@@ -1,3 +1,7 @@
 set -e
 pip freeze --path ./target/$2 > $1
-sed -i "s/dbt-core==.*/& --no-binary dbt-postgres/g" $1
+if [[ "$OSTYPE" == "darwin"* ]]; then
+ sed -i "" "s/dbt-core==.*/& --no-binary dbt-postgres/g" $1
+else
+ sed -i "s/dbt-core==.*/& --no-binary dbt-postgres/g" $1
+fi
