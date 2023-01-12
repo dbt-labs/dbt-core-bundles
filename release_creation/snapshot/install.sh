b04d6e715f60f9fad1132d9f1fae237bc1552fd9
Colin <colin.rogers@dbtlabs.com>
2023-01-12 14:36:27 -0800

    bash style improvements

--- release_creation/snapshot/install.sh
+++ release_creation/snapshot/install.sh
@@ -1,9 +1,13 @@
 set -e
 export DBT_PSYCOPG2_NAME=psycopg2
+base_dir="$1"
+requirements_file_prefix="$2"
+link_dir="$3"
+target_dir="$4"
 rm -rf target
 mkdir target 
-pip install -r $1/requirements/$2.requirements.txt \
+pip install -r $base_dir/requirements/$requirements_file_prefix.requirements.txt \
 --no-index  \
 --force-reinstall \
---find-links ./$3 \
---target ./target/$4
+--find-links ./$link_dir \
+--target ./target/$target_dir
