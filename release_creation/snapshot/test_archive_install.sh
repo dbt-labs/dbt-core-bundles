9b1e66f7dd1aa60cf3480f7b181197a9699ea878
Colin <colin.rogers@dbtlabs.com>
2023-03-03 14:37:51 -0800

    add sanity checks

--- release_creation/snapshot/test_archive_install.sh
+++ release_creation/snapshot/test_archive_install.sh
@@ -8,4 +8,19 @@ source test_archive_install/bin/activate
 python -m pip install -r "${requirements_file}" \
   --no-index \
   --find-links ./snapshot_pkg_test
+dbt --version
+# make sure psycopg2 is installed, but not psycopg2-binary
+echo -n "Checking psycopg2 install..."
+if ! pip freeze | grep psycopg2; then
+    echo "psycopg2 is not installed!"
+    exit 1
+fi
+echo ok
+
+echo -n "Checking psycopg2-binary..."
+if pip freeze | grep psycopg2-binary; then
+    echo "psycopg2-binary is installed and should not be!"
+    exit 1
+fi
+echo ok
 deactivate
