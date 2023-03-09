058300ccbf9ceaa0687177798faca5f163e7a6ee
Colin <colin.rogers@dbtlabs.com>
2023-03-09 12:56:01 -0800

    fix integ test

--- test/integration/snapshot/test_create.py
+++ test/integration/snapshot/test_create.py
@@ -10,7 +10,7 @@ import zipfile
                          argvalues=["1.1.0", "1.2.0", "1.3.0", "1.4.0", "1.5.0b1"])
 def test_generate_snapshot_creates_a_snapshot_with_valid_version(test_version):
     created_assets = generate_snapshot(Version.coerce(test_version))
-    for asset_name, asset_location in created_assets:
+    for asset_name, asset_location in created_assets.items():
         assert os.path.isfile(asset_location)
         if ".zip" in asset_name:
             assert zipfile.is_zipfile(asset_location)
