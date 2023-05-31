e096912bb4bd2ea4d9ad9843392237dcd5da1a89
Emily Rockman <emily.rockman@dbtlabs.com>
2023-05-31 11:48:40 -0500

    modify install script

--- install_snapshot.sh
+++ install_snapshot.sh
@@ -10,13 +10,9 @@ archive_file="snapshot_core_all_adapters_${suffix}.zip"
 requirements_file="snapshot_requirements_${suffix}.txt"
 echo $suffix
 export DBT_PSYCOPG2_NAME=psycopg2
-curl --fail --retry 5 --retry-all-errors -OL "${github_url}/${archive_file}"
-curl --fail --retry 5 --retry-all-errors -OL "${github_url}/${requirements_file}"
+curl --fail --retry 5 -OL "${github_url}/${archive_file}"
+curl --fail --retry 5 -OL "${github_url}/${requirements_file}"
 unzip -o "${archive_file}" -d snapshot_pkgs
-pip install -r "${requirements_file}" \
+python -m pip install -r "${requirements_file}" \
   --no-index \
-  --no-cache-dir \
-  --ignore-installed \
-  --find-links ./snapshot_pkgs \
-  --pre
-
+  --find-links ./snapshot_pkgs
