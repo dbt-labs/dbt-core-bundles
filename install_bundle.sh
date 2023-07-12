f95266611f9b2ef79c92478455854435d1bc43a7
Colin <colin.rogers@dbtlabs.com>
2023-07-12 08:47:57 -0700

    swap snapshot for bundle in install_bundle.sh

--- install_bundle.sh
+++ install_bundle.sh
@@ -5,9 +5,9 @@ created_tag="$1"
 python_version="$2"
 os_platform="$3"
 suffix="${os_platform}_${python_version}"
-github_url="https://github.com/dbt-labs/dbt-core-snapshots/releases/download/${created_tag}"
-archive_file="snapshot_core_all_adapters_${suffix}.zip"
-requirements_file="snapshot_requirements_${suffix}.txt"
+github_url="https://github.com/dbt-labs/dbt-core-bundles/releases/download/${created_tag}"
+archive_file="bundle_core_all_adapters_${suffix}.zip"
+requirements_file="bundle_requirements_${suffix}.txt"
 echo $suffix
 export DBT_PSYCOPG2_NAME=psycopg2
 curl --fail --retry 5 --retry-all-errors -OL "${github_url}/${archive_file}"
