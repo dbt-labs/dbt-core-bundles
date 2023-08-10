8a85ef2f0407abe2f59d083f2c1fd11089adb1e3
Emily Rockman <emily.rockman@dbtlabs.com>
2023-08-10 13:43:14 -0500

    drop 3.7

--- .github/scripts/supported_python_versions.sh
+++ .github/scripts/supported_python_versions.sh
@@ -1,18 +1,15 @@
 #!/bin/bash -e
 
-# 1.0 to 1.3 support python 3.7 to 3.10
+# 1.0 to 1.3 support python 3.8 to 3.10
 # 1.4 added support for python 3.11
-# 1.6 drops support for python 3.7
 # Note that Python 3.8 is manually added to the matrix so not included here
 
 minor_version="$1"
 if [[ ${minor_version} < 4 ]]
 then
-py_versions='["3.7", "3.8", "3.9", "3.10"]'
+py_versions='["3.8", "3.9", "3.10"]'
 elif  [[ ${minor_version} < 6 ]]
 then
-py_versions='["3.7", "3.8", "3.9", "3.10", "3.11"]'
-else
 py_versions='["3.8", "3.9", "3.10", "3.11"]'
 fi
 echo $py_versions
