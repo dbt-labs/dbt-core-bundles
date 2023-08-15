698207af3b9e32044a07ee4a0b6094019953e58b
Colin <colin.rogers@dbtlabs.com>
2023-08-15 11:36:30 -0700

    add backoff

--- release_creation/github_client/client.py
+++ release_creation/github_client/client.py
@@ -2,12 +2,13 @@ import os
 
 from github import Github
 from github.Repository import Repository
+from urllib3 import Retry
 
 _GH_BUNDLE_REPO = "dbt-labs/dbt-core-bundles"
-
+_RETRY = Retry(total=5, backoff_factor=0.2)
 
 def get_github_client() -> Github:
-    return Github(os.environ.get("GH_ACCESS_TOKEN"))
+    return Github(os.environ.get("GH_ACCESS_TOKEN"), retry=_RETRY)
 
 
 def get_bundle_repo() -> Repository:
