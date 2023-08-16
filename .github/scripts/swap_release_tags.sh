663ef69e40f4d1d5a6dffcf7dd75863ee7aba749
Colin <colin.rogers@dbtlabs.com>
2023-08-16 09:41:11 -0700

    add swap release script

--- .github/scripts/swap_release_tags.sh
+++ .github/scripts/swap_release_tags.sh
@@ -4,8 +4,9 @@ set -e
 swap_from_tag="$1"
 swap_to_tag="$2"
 
-swap_from_release=$(gh release view "${swap_from_tag}")
-swap_to_release=$(gh release view "${swap_to_tag}")
+swap_from_release=$(gh release view "${swap_from_tag}") #0.0.0
+swap_to_release=$(gh release view "${swap_to_tag}") #0.0.1
+swap_to_release_temp="${swap_to_release}.temp"
 
 if [ -z "$swap_from_release" ]; then
   echo "Release with tag $swap_from_tag does not exist."
@@ -17,5 +18,6 @@ if [ -z "$swap_to_release" ]; then
   exit 1
 fi
 
-gh release edit "${swap_from_tag}" --tag"${swap_to_release}"
-gh release edit "${swap_from_tag}" --tag"${swap_to_release}"
+gh release edit "${swap_to_release}" --tag "${swap_to_release_temp}"
+gh release edit "${swap_from_release}" --tag "${swap_to_release}"
+gh release edit "${swap_to_release_temp}" --tag "${swap_from_release}"
