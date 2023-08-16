04d168d9e94b37f2cf254301d4d3b09b0942c6d1
Colin <colin.rogers@dbtlabs.com>
2023-08-16 16:02:32 -0700

    add libopenblas to install script

--- .github/scripts/worker_install_linux_deps.sh
+++ .github/scripts/worker_install_linux_deps.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 set -e
 
-sudo apt-get install libsasl2-dev libxml2-dev libxslt-dev gcc g++ unixodbc-dev
-sudo apt-get install gfortran libatlas-base-dev
+sudo apt-get install libsasl2-dev \
+                      libxml2-dev \
+                      libxslt-dev \
+                      gcc \
+                      g++ \
+                      unixodbc-dev \
+                      libopenblas-dev
