1e1810ef6569a57ddb95cc1ccd8d33dfc364aca1
Kshitij Aranke <kshitij.aranke@dbtlabs.com>
2024-02-26 17:40:28 -0600

    change branching logic

--- .github/scripts/supported_python_versions.sh
+++ .github/scripts/supported_python_versions.sh
@@ -2,16 +2,20 @@
 
 # 1.0 to 1.3 support python 3.8 to 3.10
 # 1.4 added support for python 3.11
+# 1.7 added support for python 3.12
 
 major_version="$1"
 minor_version="$2"
 # if major.minor is 0.0 that is a dev release
-if [[ ${minor_version} == 0 && ${major_version} == 0 ]]
+if [[ ${major_version} == 0 && ${minor_version} == 0 ]]
 then
 py_versions='["3.8", "3.9", "3.10", "3.11", "3.12"]'
-elif [[ ${minor_version} < 5 && ${major_version} == 1  ]]
+elif [[ ${major_version} == 1 && ${minor_version} < 4  ]]
 then
 py_versions='["3.8", "3.9", "3.10"]'
+elif [[ ${major_version} == 1 && ${minor_version} > 3 && ${minor_version} < 7 ]]
+then
+py_versions='["3.8", "3.9", "3.10", "3.11"]'
 else
 py_versions='["3.8", "3.9", "3.10", "3.11", "3.12"]'
 fi
