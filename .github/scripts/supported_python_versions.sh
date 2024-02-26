6723733ee792a0c8dfe693d2f76d7bab5acbbccb
Kshitij Aranke <kshitij.aranke@dbtlabs.com>
2024-02-26 11:12:43 -0600

    Add Python 3.12 to supported versions

--- .github/scripts/supported_python_versions.sh
+++ .github/scripts/supported_python_versions.sh
@@ -8,12 +8,12 @@ minor_version="$2"
 # if major.minor is 0.0 that is a dev release
 if [[ ${minor_version} == 0 && ${major_version} == 0 ]]
 then
-py_versions='["3.8", "3.9", "3.10", "3.11"]'
+py_versions='["3.8", "3.9", "3.10", "3.11", "3.12"]'
 elif [[ ${minor_version} < 5 && ${major_version} == 1  ]]
 then
 py_versions='["3.8", "3.9", "3.10"]'
 else
-py_versions='["3.8", "3.9", "3.10", "3.11"]'
+py_versions='["3.8", "3.9", "3.10", "3.11", "3.12"]'
 fi
 echo $py_versions
 echo "versions=$py_versions" >> "$GITHUB_OUTPUT"
