03dd64d83086444fa79b8de41aa5140ac5eb8ac9
Mike Alfare <mike.alfare@dbtlabs.com>
2024-03-13 18:29:30 -0400

    remove formatting changes

--- release_creation/bundle/test_archive_install.sh
+++ release_creation/bundle/test_archive_install.sh
@@ -9,7 +9,6 @@ unzip -o "${archive_file}" -d bundle_pkg_test
 rm -rf test_archive_install
 python -m venv test_archive_install
 source test_archive_install/bin/activate
-
 python -m pip install -r "${requirements_file}" \
   --no-index \
   --find-links ./bundle_pkg_test \
