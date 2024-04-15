ee1caa135d05a16245955e3cd541f0c5c1613875
Mike Alfare <13974384+mikealfare@users.noreply.github.com>
2024-04-15 18:29:14 -0400

    Fix psycopg2 install (#139)
    * add psycopg2 env variable
    * add a test for the download process to ensure we always get the correct version of psycopg2
    * align current and 1.8 req files
    * limit psycopg2 check to linux
    * update download test to be platform based
    * remove 1.8.0b1 from tests since we know this does not work with the new method (or the old)

---
+++ test/integration/bundle/test_download.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+import platform
+from semantic_version import Version
+import sys
+
+import pytest
+
+from release_creation.bundle.bundle_config import get_bundle_config
+from release_creation.bundle.create import _download_packages
+
+
+@pytest.mark.parametrize("test_version", ["0.0.0"])
+def test_correct_version_of_psycopg2(test_version):
+    if sys.version_info == (3, 10) and Version.coerce(test_version) >= Version.coerce(
+        "1.5.0"
+    ):
+        pytest.skip("We run test for Python 3.11 with version 1.5.0+ of the bundle")
+
+    bundle_configuration = get_bundle_config(Version.coerce(test_version))
+    _download_packages(bundle_configuration)
+
+    tmp_dir = Path(bundle_configuration.py_version_tmp_path)
+    assert tmp_dir.is_dir()
+    psycopg2_is_found = False
+    psycopg2_binary_is_found = False
+    for file in tmp_dir.iterdir():
+        if "psycopg2" in file.name:
+            if "psycopg2_binary" not in file.name:
+                psycopg2_is_found = True
+            else:
+                psycopg2_binary_is_found = True
+    if platform.system() == "Linux":
+        assert psycopg2_is_found and not psycopg2_binary_is_found
+    elif platform.system() == "Darwin":
+        assert psycopg2_binary_is_found and not psycopg2_is_found
+    else:
+        assert False, f"Unexpected platform: {platform.system()}"
