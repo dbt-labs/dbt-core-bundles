20a635c1c32ffa7474706d2e67a0c3ef8f96562b
Mike Alfare <mike.alfare@dbtlabs.com>
2024-04-15 11:11:41 -0400

    limit psycopg2 check to linux

--- release_creation/bundle/test_archive_install.sh
+++ release_creation/bundle/test_archive_install.sh
@@ -14,16 +14,16 @@ python -m pip install -r "${requirements_file}" \
   --find-links ./bundle_pkg_test \
   --pre
 dbt --version
-# make sure psycopg2 is installed, but not psycopg2-binary
-echo -n "Checking psycopg2 install..."
-if ! pip freeze | grep psycopg2; then
+# make sure psycopg2 is installed for linux, but not psycopg2-binary
+echo -n "Checking psycopg2 install for linux..."
+if [[ "$OSTYPE" == linux* && ! pip freeze | grep psycopg2 ]]; then
     echo "psycopg2 is not installed!"
     exit 1
 fi
 echo ok
 
-echo -n "Checking psycopg2-binary..."
-if pip freeze | grep psycopg2-binary; then
+echo -n "Checking psycopg2-binary for linux..."
+if [[ "$OSTYPE" == linux* && pip freeze | grep psycopg2-binary ]]; then
     echo "psycopg2-binary is installed and should not be!"
     exit 1
 fi
