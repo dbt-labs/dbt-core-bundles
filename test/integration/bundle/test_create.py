7cd2ed1fcc9b3bf40f91ca258eecf1bdc7143c42
Mike Alfare <mike.alfare@dbtlabs.com>
2024-04-15 11:28:19 -0400

    revert formatting change

--- test/integration/bundle/test_create.py
+++ test/integration/bundle/test_create.py
@@ -9,19 +9,10 @@ import os.path
 import zipfile
 
 
-TEST_VERSIONS = [
-    "0.0.0",
-    "1.3.0",
-    "1.4.0",
-    "1.5.0",
-    "1.6.0",
-    "1.7.0b1",
-    "1.7.0",
-    "1.8.0b1",
-]
-
-
-@pytest.mark.parametrize("test_version", TEST_VERSIONS)
+@pytest.mark.parametrize(
+    argnames="test_version",
+    argvalues=["0.0.0", "1.3.0", "1.4.0", "1.5.0", "1.6.0", "1.7.0b1", "1.7.0", "1.8.0b1"],
+)
 def test_generate_bundle_creates_a_bundle_with_valid_version(
     test_version
 ):
